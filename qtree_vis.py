ad6d7c0c2d133ecf782e51acfddb00a5a5675fe9
Vamsi Subraveti <vis@ornl.gov>
2025-12-08 18:50:16 -0600

    updated profiling

--- qtree_vis.py
+++ qtree_vis.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from core import insert_particle
 from structures import FlatQTree
+import time
 
 generator = np.random.default_rng(42)
 particles = generator.random(size=(2000,2))  # 10 random 2D particles
@@ -12,10 +13,11 @@ radii = np.power(masses, 1/3) * k  # radii proportional to cube root of mass
 plt.scatter(particles[:,0], particles[:,1], s=1, color='blue')
 
 qtree = FlatQTree(max_nodes=2000)
+s = time.time()
 qtree.reset(0.5, 0.5, 1.0)
 for i in range(particles.shape[0]):
     insert_particle(qtree, particles[:,0], particles[:,1], i)
-
+print("Tree build time:", time.time() - s)
 for node in range(qtree.node_count):
     x, y, size = qtree.bbox[node]
     rect = plt.Rectangle((x-size/2, y-size/2), size, size, fill=False, edgecolor='r', linewidth=0.5)
--- run.py
+++ run.py
@@ -5,15 +5,32 @@ from numba import njit, int64, float64
 import time
 
 generator = np.random.default_rng(42)
-particles = generator.random(size=(1000000,2))  # 1000000 random 2D particles
-masses = np.random.lognormal(size=(1000000,))  # random masses
+particles = generator.random(size=(50000,2))  # 1000000 random 2D particles
+masses = np.random.lognormal(size=(50000,))  # random masses
 k = 5.0
 radii = np.power(masses, 1/3) * k  # radii proportional to cube root of mass
 tsteps = 100
 orchestrator = Orchestrator(particles, masses, G=1.0, theta=0.5, softening=0.01)
 
-for _ in range(tsteps):
-    s = time.time()
+bavg, aavg, cavg = 0.0, 0.0, 0.0
+for t in range(tsteps):
+    s1 = time.time()
+    orchestrator.build_tree()
+    e1 = time.time()
+    
+    orchestrator.aggregate()
+    e2 = time.time()
+
+
     particles = orchestrator.integrate(dt=0.01)
-    e = time.time()
-print("Average time per step:", (e - s)/tsteps)
+    e3 = time.time()
+
+    if t>0:
+        bavg += e1 - s1
+        aavg += e2 - e1
+        cavg += e3 - e2
+print(f"Average build time: {bavg / (tsteps-1):.6f} s")
+print(f"Average aggregate time: {aavg / (tsteps-1):.6f} s")
+print(f"Average compute+integrate time: {cavg / (tsteps-1):.6f} s")
+print(f"Total average time per step: {(bavg + aavg + cavg) / (tsteps-1):.6f} s")
+
